c3a185f59265c179c8ba5a9e8b15c94b3abe777f
Manas Choudhary <mannchoudhary3431@gmail.com>
2024-09-23 20:30:40 +0530

    inc

---
+++ Arrays/SecondLargest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[100], n;
+    cout << "enter the size of an array:" << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
